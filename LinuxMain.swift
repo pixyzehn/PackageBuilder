f0b668f9f0ddd42c55b99f0b5f625f61ed10ca76
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-05-20 01:03:26 +0900

    Add Templates/LinuxMain.swift

---
+++ LinuxMain.swift
@@ -0,0 +1,9 @@
+//
+//  LinuxMain.swift
+//  PackageBuilder
+//
+//  Created by pixyzehn on 2017/05/20.
+//
+//
+
+import Foundation
---
+++ Templates/LinuxMain.swift
@@ -0,0 +1,12 @@
+/**
+ *  {PROJECT_NAME}
+ *  Copyright (c) {YOUR_NAME} {THIS_YEAR}
+ *  Licensed under the MIT license. See LICENSE file.
+ */
+
+import XCTest
+@testable import {PROJECT_NAME}Tests
+
+XCTMain([
+    testCase({PROJECT_NAME}Tests.allTests),
+])
